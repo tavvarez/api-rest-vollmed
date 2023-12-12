776387c68d1e39262f0f9ae9730a722348334565
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-12-11 22:31:40 -0300

    refactor package domain

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import med.voll.api.domain.endereco.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
