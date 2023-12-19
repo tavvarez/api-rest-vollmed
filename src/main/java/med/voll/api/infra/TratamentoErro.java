268e2686f65e54585b5b2227b81c90341b1b4e59
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-12-19 20:36:40 -0300

    feat: tratamento de erro 400

--- src/main/java/med/voll/api/infra/TratamentoErro.java
+++ src/main/java/med/voll/api/infra/TratamentoErro.java
@@ -1,6 +1,8 @@
 package med.voll.api.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.persistence.EntityNotFoundException;
@@ -8,7 +10,20 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class TratamentoErro {
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratamentoDeErro() {
+    public ResponseEntity tratamentoDeErro404() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratamentoDeErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
